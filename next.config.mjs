b14f69bcb24474868f23f896894bffaf7c4e5345
Oluwatobiloba Odukoya <todukoya50@gmail.com>
2025-10-26 19:44:04 +0100

    Change module export to ES6 syntax

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/Projects', // if repo is not a user/org site
   images: {
     unoptimized: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
